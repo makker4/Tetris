d850f54acdff6bc107826cff710cde0a7f5a56c7
Mads Caspersen <mads.caspersen@keylane.com>
2025-03-11 16:26:48 +0100

    Remove completed blocks and Game Over

---
+++ Lesson 14 - Game over/main.py
@@ -0,0 +1,43 @@
+import pygame
+import sys
+from game import Game
+
+pygame.init()
+dark_blue = (44, 44, 127)
+
+screen = pygame.display.set_mode((300, 600))
+pygame.display.set_caption("Python Tetris")
+
+clock = pygame.time.Clock()
+
+game = Game()
+
+GAME_UPDATE = pygame.USEREVENT
+pygame.time.set_timer(GAME_UPDATE, 200)
+
+while True:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      pygame.quit()
+      sys.exit()
+    if event.type == pygame.KEYDOWN:
+      if game.game_over == True:
+        game.game_over = False
+        game.reset()
+      if event.key == pygame.K_LEFT and game.game_over == False:
+        game.move_left()
+      if event.key == pygame.K_RIGHT and game.game_over == False:
+        game.move_right()
+      if event.key == pygame.K_DOWN and game.game_over == False:
+        game.move_down()
+      if event.key == pygame.K_UP and game.game_over == False:
+        game.rotate()
+    if event.type == GAME_UPDATE and game.game_over == False:
+      game.move_down()
+  
+  # Drawing
+  screen.fill(dark_blue)
+  game.draw(screen)
+
+  pygame.display.update()
+  clock.tick(60)
